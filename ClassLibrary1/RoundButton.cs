ef971183634de6359348663adbd7a7fce128e312
Nezaket Kaya <94640744+nezaketkaya@users.noreply.github.com>
2023-09-16 15:51:01 +0300

    Add files via upload

---
+++ ClassLibrary1/RoundButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class RoundButton : Button
+{
+    public RoundButton()
+    {
+        this.FlatStyle = FlatStyle.Flat;
+        this.FlatAppearance.BorderSize = 0;
+        this.Size = new Size(100, 100);
+        this.BackColor = Color.White;
+        this.ForeColor = Color.Black;
+        this.Text = "Round Button";
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        GraphicsPath path = new GraphicsPath();
+        path.AddEllipse(0, 0, this.Width, this.Height);
+
+        this.Region = new Region(path);
+    }
+}
